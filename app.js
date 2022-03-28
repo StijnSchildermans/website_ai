ebd1d457d8233289df9774154b2979bb1406dff5
StijnSchildermans <32482490+StijnSchildermans@users.noreply.github.com>
2022-03-28 17:22:01 +0200

    Ongeveer af

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const path = require('path');
+var nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
 
 const app = new express();
 const port = process.env.PORT || "8000";
+
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.get('/', function(request,response) {
   response.sendFile(path.join(__dirname, '/index.html'));
@@ -22,6 +28,35 @@ app.get('/vacatures', function(request,response) {
 
 app.get('/resources/:file', function(request,response) {
   response.sendFile(path.join(__dirname, '/resources/' + request.params.file));
+});
+
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'website.questions.remarks@gmail.com',
+    pass: 'Paddenstoel586'
+  }
+});
+
+app.post('/sendemail', function(request,response) {
+  var naam = request.body.Name;
+  var email = request.body.Email;
+  var subject = request.body.Subject;
+  var body = request.body.Body;
+  var mailOptions = {
+    from: email,
+    to: 'georges@a-i-electro-technics.be',
+    subject: subject,
+    text: "Afzender: " + naam + "\nE-mailadres: " + email + "\n\n Vraag/opmerking:\n" + body
+  };
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
 });
 
 
